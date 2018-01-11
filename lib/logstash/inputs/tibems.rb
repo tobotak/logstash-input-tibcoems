2a6438baac9eac0766fe50dcef5ac4d30c36e24b
tobotak <baron5002@gmail.com>
2018-01-11 05:07:20 -0800

    added jars

--- lib/logstash/inputs/tibems.rb
+++ lib/logstash/inputs/tibems.rb
@@ -4,42 +4,154 @@ require "logstash/namespace"
 require "stud/interval"
 require "socket" # for Socket.gethostname
 
+require "logstash/event"
+require "rubygems"
+require 'jms-2.0.jar'
+require 'tibjms.jar'
+require 'tibjmsadmin.jar'
+require 'tibcrypt.jar'
+
 # Generate a repeating message.
 #
 # This plugin is intented only as an example.
 
 class LogStash::Inputs::Tibems < LogStash::Inputs::Base
-  config_name ""
+  config_name "tibems"
 
   # If undefined, Logstash will complain, even if codec is unused.
-  default :codec, "plain"
+  default :codec, "json"
 
   # The message string to use in the event.
-  config :message, :validate => :string, :default => "Hello World!"
 
-  # Set how frequently messages should be sent.
-  #
-  # The default, `1`, means send a message every second.
-  config :interval, :validate => :number, :default => 1
+  config :user, :validate => :string, :required => true
+  config :password , :validate => :string, :required => true
+  config :queueName, :validate => :string, :required => true
+  config :initialContextFactory, :validate => :string, :required => true
+  config :providerUrl, :validate => :string, :required => true
+  config :queueConnectionFactory, :validate => :string, :requierd => true
+
+  def initialize(*args)
+    require "java"
+    super(*args)
+  end # def initialize
 
   public
   def register
-    @host = Socket.gethostname
+    @logger.info("opening connection to EMS server.", :providerUrl => @providerUrl);
+
+    env = Hashtable.new
+    env.put javax.naming.Context.INITIAL_CONTEXT_FACTORY,@initialContextFactory
+    env.put javax.naming.Context.PROVIDER_URL,@providerUrl
+    env.put javax.naming.Context.SECURITY_PRINCIPAL,@user
+    env.put javax.naming.Context.SECURITY_CREDENTIALS,@password
+
+    begin
+      jndiContext = javax.naming.InitialContext.new(env) ;
+      env = jndiContext.getEnvironment();
+      connectionFactory = jndiContext.lookup(@queueConnectionFactory);
+      @connection = connectionFactory.createQueueConnection(@user,@password);
+      @session = @connection.createQueueSession(false,@ackMode);
+      @queue = @session.createQueue(@queueName);
+      @receiver = @session.createReceiver(@queue);
+      @connection.start();
+    rescue Exception => e
+      @logger.error("Error initalizing jms input", :exception => e, :backtrace => e.backtrace)
+      raise
+    end
+      @logger.info("Connection established");
   end # def register
 
+
+
   def run(queue)
-    # we can abort the loop if stop? becomes true
-    while !stop?
-      event = LogStash::Event.new("message" => @message, "host" => @host)
-      decorate(event)
-      queue << event
-      # because the sleep interval can be big, when shutdown happens
-      # we want to be able to abort the sleep
-      # Stud.stoppable_sleep will frequently evaluate the given block
-      # and abort the sleep(@interval) if the return value is true
-      Stud.stoppable_sleep(@interval) { stop? }
-    end # loop
-  end # def run
+
+    @thread = Thread.current
+
+    begin
+      while !@interrupted do
+        msg = @receiver.receive()
+        @logger.debug("Received message", :message => msg)
+        @logger.debug("Received JMS message", :CorrelationID => msg.getJMSCorrelationID())
+        @logger.debug("Received JMS message", :DeliveryMode => msg.getJMSDeliveryMode())
+        @logger.debug("Received JMS message", :MessageID => msg.getJMSMessageID())
+        @logger.debug("Received JMS message", :JMSPriority => msg.getJMSPriority())
+        @logger.debug("Received JMS message", :JMSDestination => msg.getJMSDestination())
+        @logger.debug("Received JMS message", :JMSType =>  msg.getJMSType())
+
+        @logger.debug("Received JMS message", :JMSReplyTo => msg.getJMSReplyTo())
+        @logger.debug("Received JMS message", :JMSPriority => msg.getJMSPriority())
+        @logger.debug("Received JMS message", :JMSRedelivered => msg.getJMSRedelivered())
+        @logger.debug("Received JMS message", :JMSExpiration => msg.getJMSExpiration())
+
+        if msg.java_kind_of?(javax.jms.TextMessage)
+          @logger.debug("Received Text message", :body => msg.getText())
+
+          @codec.decode(msg.getText())
+          do |event|
+            decorate(event)
+            event.timestamp = Time.at(msg.getJMSTimestamp()/1000)
+            event["JMSCorrelationID"] = msg.getJMSCorrelationID()
+            event["JMSMessageID"] = msg.getJMSMessageID()
+            event["JMSDestination"] = msg.getJMSDestination().toString()
+            event["messageType"] = "TextMessage"
+            output_queue << event
+          end
+        elsif msg.java_kind_of?(javax.jms.MapMessage)
+          @logger.debug("Received Map message", :body => msg.getString("_cl.msg"))
+          @codec.decode(msg.getString("_cl.msg"))
+          do |event|
+            decorate(event)
+            time = Time.at(msg.getJMSTimestamp()/1000)
+            event.timestamp = time
+            m = msg.getJMSTimestamp()%1000
+            event["timestampFormated"] = time.strftime("%Y-%m-%d %H:%M:%S")+","+m.to_s
+            event["JMSTimestamp"] = msg.getJMSTimestamp()
+
+            event["JMSCorrelationID"] = msg.getJMSCorrelationID()
+            event["JMSMessageID"] = msg.getJMSMessageID()
+            event["JMSDestination"] =msg.getJMSDestination().toString()
+            event["messageType"] = "MapMessage"
+
+            event["globalInstanceId"] = msg.getString("_cl.globalInstanceId")
+            event["severity"] = msg.getString("_cl.severity")
+            event["expirationTimeInDB"] = msg.getString("_cl.expirationTimeInDB")
+            event["contextId"] =  msg.getString("_cl.contextId")
+            event["physicalCompId.scheme"] =  msg.getString("_cl.physicalCompId.scheme")
+            event["priority"] = msg.getString("_cl.priority")
+            event["creationTimes"] =  msg.getString("_cl.creationTimes")
+            event["reportingCompId.scheme"] = msg.getString("_cl.reportingCompId.scheme")
+            event["reportingCompId.hierarchyname"] =  msg.getString("_cl.reportingCompId.hierarchyname")
+            event["logicalCompId.scheme"] = msg.getString("_cl.logicalCompId.scheme")
+            event["physicalCompId.matrix.node"] = msg.getString("_cl.physicalCompId.matrix.node")
+            event["ApplicationName"] = msg.getString("_cl.logicalCompId.matrix.application")
+            event["correlationId"] =  msg.getString("_cl.correlationId")
+            event["logicalCompId.matrix.operation"] = msg.getString("_cl.logicalCompId.matrix.operation")
+            event["physicalCompId.matrix.env"] =  msg.getString("_cl.physicalCompId.matrix.env")
+            event["msgId"] =  msg.getString("_cl.msgId")
+            event["nanoTime"] = msg.getString("_cl.nanoTime")
+            event["reportingCompId.value"] =  msg.getString("_cl.reportingCompId.value")
+            event["locationId"] = msg.getString("_cl.locationId")
+
+            output_queue << event
+          end
+        else
+          @logger.debug("JMS message type not supported!")
+        end
+        msg.acknowledge() if [2, 23, 23].include? @ackMode
+      end # loop
+      rescue Exception => e
+      @logger.error("Error when receiving message", :exception => e, :backtrace => e.backtrace)
+    end
+  end # ond of run procedure
+
+  def teardown
+    @interrupted = true
+    @receiver.close()
+    @session.close()
+    @connection.close()
+    finished
+    @thread.raise(Interrupted)
+  end # def teardown
 
   def stop
     # nothing to do in this case so it is not necessary to define stop
--- logstash-input-tibems.gemspec
+++ logstash-input-tibems.gemspec
@@ -2,17 +2,23 @@ Gem::Specification.new do |s|
   s.name          = 'logstash-input-tibems'
   s.version       = '0.1.0'
   s.licenses      = ['Apache-2.0']
-  s.summary       = 'TODO: Write a short summary, because Rubygems requires one.'
+  s.summary       = 'this is logstash input plugin to receive messages from tibco ems queues'
   s.description   = '{TODO: Write a longer description or delete this line.'
-  s.homepage      = 'TODO: Put your plugin''s website or public repo URL here.'
-  s.authors       = ['']
-  s.email         = ''
+  s.homepage      = 'https://github.com/tobotak/logstash-input-tibcoems'
+  s.authors       = ['tobotak for cgi use']
+  s.email         = 'baron5002@gmail.com'
   s.require_paths = ['lib']
 
   # Files
   s.files = Dir['lib/**/*','spec/**/*','vendor/**/*','*.gemspec','*.md','CONTRIBUTORS','Gemfile','LICENSE','NOTICE.TXT']
    # Tests
   s.test_files = s.files.grep(%r{^(test|spec|features)/})
+
+  # plugin needs tibco ems jar files
+  s.files << 'lib/jms-2.0.jar'
+  s.files << 'lib/tibcrypt.jar'
+  s.files << 'lib/tibjms.jar'
+  s.files << 'lib/tibjmsadmin.jar'
 
   # Special flag to let us know this is actually a logstash plugin
   s.metadata = { "logstash_plugin" => "true", "logstash_group" => "input" }
