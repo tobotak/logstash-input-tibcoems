0929b64def24fcd36d896cc9e6f17c42fcc60c48
tobotak <baron5002@gmail.com>
2018-01-25 00:07:47 +0100

    Change codec, change type of getting msgs

--- lib/logstash/inputs/tibems.rb
+++ lib/logstash/inputs/tibems.rb
@@ -19,9 +19,10 @@ class LogStash::Inputs::Tibems < LogStash::Inputs::Base
   config_name "tibems"
 
   # If undefined, Logstash will complain, even if codec is unused.
-  default :codec, "plain"
+  default :codec, "json"
 
   # The message string to use in the event.
+
 
   config :user, :validate => :string, :required => true
   config :password , :validate => :string, :required => true
@@ -29,6 +30,7 @@ class LogStash::Inputs::Tibems < LogStash::Inputs::Base
   config :initialContextFactory, :validate => :string, :required => true
   config :providerUrl, :validate => :string, :required => true
   config :queueConnectionFactory, :validate => :string, :requierd => true
+  config :ackMode, :validate => :string, :requierd => true
 
   def initialize(*args)
     require "java"
@@ -38,28 +40,38 @@ class LogStash::Inputs::Tibems < LogStash::Inputs::Base
   public
   def register
     @logger.info("opening connection to EMS server.", :providerUrl => @providerUrl);
-
-#    env = Hashtable.new
-#    env.put javax.naming.Context.INITIAL_CONTEXT_FACTORY,@initialContextFactory
-#    env.put javax.naming.Context.PROVIDER_URL,@providerUrl
-#    env.put javax.naming.Context.SECURITY_PRINCIPAL,@user
-#    env.put javax.naming.Context.SECURITY_CREDENTIALS,@password
-
     begin
-      @factory = javax.jms.ConnectionFactory;
       @connection = javax.jms.Connection;
       @session = javax.jms.Session;
       @queue = javax.jms.queue;
       @receiver = javax.jms.MessageConsumer;
+      if @ackMode == "AUTO"
+        @ack = javax.jms.Session.AUTO_ACKNOWLEDGE; # value 1
+      elsif @ackMode == "CLIENT"
+        @ack = javax.jms.Session.CLIENT_ACKNOWLEDGE; # value 2
+      elsif @ackMode == "DUPS_OK"
+        @ack = javax.jms.Session.DUPS_OK_ACKNOWLEDGE; # value 3
+      elsif @ackMode == "EXPLICIT_CLIENT"
+        @ack = com.tibco.tibjms.Tibjms.EXPLICIT_CLIENT_ACKNOWLEDGE; # value 23
+      elsif @ackMode == "EXPLICIT_CLIENT_DUPS_OK"
+        @ack = com.tibco.tibjms.Tibjms.EXPLICIT_CLIENT_DUPS_OK_ACKNOWLEDGE; # value 24
+      elsif @ackMode == "NO"
+        @ack = com.tibco.tibjms.Tibjms.NO_ACKNOWLEDGE; # value 22
+      else
+        System.err.println("Unrecognized -ackmode: ", :ackmode => @ackMode)
+      end # end of if
       @logger.debug("========== factory definited")
       @factory = com.tibco.tibjms.TibjmsConnectionFactory.new(@providerUrl);
       @logger.debug("========== factory initialized")
       @logger.debug("========== initailizating connection with:", :user => @user, :password => @password)
       @connection = @factory.createConnection(@user,@password);
       @logger.debug("========== connection created ", @connection)
- #     @session = @connection.createSession(@ackMode);
- #     @queue = @session.createQueue(@queueName);
- #     @receiver = @session.createConsumer(@queue);
+      @session = @connection.createSession(false,@ack);
+      @logger.debug("========== session created")
+      @queue = @session.createQueue(@queueName);
+      @logger.debug("========== queue ref created", :queuename => @queue.getQueueName())
+      @receiver = @session.createConsumer(@queue);
+      @logger.debug("========== receiver created")
       @connection.start();
       @logger.debug("========= connection ID:", :clientID => @connection.getClientID())
     rescue Exception => e
@@ -74,39 +86,40 @@ class LogStash::Inputs::Tibems < LogStash::Inputs::Base
   def run(queue)
 
     @thread = Thread.current
-
     begin
       while !@interrupted do
         msg = @receiver.receive()
-        @logger.debug("Received message", :message => msg)
+        @logger.info("Received message", :messageI => msg)
         @logger.debug("Received JMS message", :CorrelationID => msg.getJMSCorrelationID())
         @logger.debug("Received JMS message", :DeliveryMode => msg.getJMSDeliveryMode())
         @logger.debug("Received JMS message", :MessageID => msg.getJMSMessageID())
         @logger.debug("Received JMS message", :JMSPriority => msg.getJMSPriority())
         @logger.debug("Received JMS message", :JMSDestination => msg.getJMSDestination())
         @logger.debug("Received JMS message", :JMSType =>  msg.getJMSType())
-
         @logger.debug("Received JMS message", :JMSReplyTo => msg.getJMSReplyTo())
         @logger.debug("Received JMS message", :JMSPriority => msg.getJMSPriority())
         @logger.debug("Received JMS message", :JMSRedelivered => msg.getJMSRedelivered())
         @logger.debug("Received JMS message", :JMSExpiration => msg.getJMSExpiration())
+        @logger.debug("Received JMS message", :JMSTimestamp=> msg.getJMSTimestamp())
 
         if msg.java_kind_of?(javax.jms.TextMessage)
           @logger.debug("Received Text message", :body => msg.getText())
-
+          event = event = LogStash::Event.new()
+          event.set("@timestamp", LogStash::Timestamp.at(msg.getJMSTimestamp() / 1000, (msg.getJMSTimestamp() % 1000) * 1000))
           @codec.decode(msg.getText()) do |event|
+            event.set("JMSCorrelationID", msg.getJMSCorrelationID())
+            event.set("JMSMessageID", msg.getJMSMessageID())
+            event.set("JMSDestination", msg.getJMSDestination().toString())
+            event.set("messageType", "TextMessage")
+            event.set("messageBody", msg.getText())
+            event.set("timestamp", msg.getJMSTimestamp())
             decorate(event)
-            event.timestamp = Time.at(msg.getJMSTimestamp()/1000)
-            event["JMSCorrelationID"] = msg.getJMSCorrelationID()
-            event["JMSMessageID"] = msg.getJMSMessageID()
-            event["JMSDestination"] = msg.getJMSDestination().toString()
-            event["messageType"] = "TextMessage"
-            output_queue << event
+            queue << event
           end
         else
-          @logger.debug("JMS message type not supported!")
+          @logger.error("JMS message type not supported!")
         end
-        msg.acknowledge() if [2, 23, 23].include? @ackMode
+        msg.acknowledge() if [2, 23, 24].include? @ack
       end # loop
       rescue Exception => e
       @logger.error("Error when receiving message", :exception => e, :backtrace => e.backtrace)
